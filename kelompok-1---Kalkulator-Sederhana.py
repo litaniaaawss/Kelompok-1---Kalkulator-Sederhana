9af41f6b0c78593ed2dfe363f25df9668a6f6d05
Litania <164005815+litaniaaawss@users.noreply.github.com>
2025-11-25 13:39:50 +0800

    Update kelompok-1---Kalkulator-Sederhana.py

---
+++ kelompok-1---Kalkulator-Sederhana.py
@@ -0,0 +1,25 @@
+# Kelompok 1
+# Vikenza Slat 250211060014
+# Litania Supit 250211060018
+# Syaloom Keintjem 250211060027
+# Kristania Manorek 2502110600
+
+#---Kalkulator Sederhana---
+
+def penjumlahan(a, b):
+    return a + b
+
+def pengurangan(a, b):
+    return a - b
+
+def perkalian(a, b):
+    return a * b
+
+def pembagian(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "pembagian tidak bisa menggunakan 0"
+
+print("---Kalkulator Sederhana---")
+print("--------------------------")
